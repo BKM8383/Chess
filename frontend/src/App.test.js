fa2f5f56d54cd62e42bea7108ff88e496b410c62
Bhupendra kumawat <122453339+BKM8383@users.noreply.github.com>
2025-04-21 16:42:45 +0530

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+global.WebSocket = function () {
+  return {
+    send: () => {},
+    close: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+  };
+};
+
 test('basic test to keep CI green', () => {
   expect(true).toBe(true);
 });
