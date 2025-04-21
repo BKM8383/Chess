76ac3d462452c7e8ae7f0e1484cadb85fb4bb796
Bhupendra kumawat <122453339+BKM8383@users.noreply.github.com>
2025-04-21 16:45:12 +0530

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,15 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-global.WebSocket = function () {
-  return {
-    send: () => {},
-    close: () => {},
-    addEventListener: () => {},
-    removeEventListener: () => {},
-  };
-};
-
-test('basic test to keep CI green', () => {
-  expect(true).toBe(true);
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
